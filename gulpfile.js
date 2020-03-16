b10fe04b7176ad190268f13248a0be3549fc5bcd
Nic Raboy <nraboy@gmail.com>
2020-03-16 14:05:30 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const { src, series } = require("gulp");
+const clean = require("gulp-clean");
+const shell = require("gulp-shell");
+
+function cleanTask() {
+    return src(["./hosting/files", "./public"], { read: false, allowEmpty: true })
+        .pipe(clean());
+}
+
+function generateTask() {
+    return src("./hugo", { read: true })
+        .pipe(shell(["hugo --config <%= file.path %>/config.toml --themesDir <%= file.path %>/themes/"]));
+}
+
+function deployTask() {
+    return src("./hosting", { read: true })
+        .pipe(shell([
+            `stitch-cli login --private-api-key=${process.env.STITCH_PRIVATE_API_KEY} --api-key=${process.env.STITCH_API_KEY} --yes`,
+            `stitch-cli import --include-hosting --yes`
+        ]));
+}
+
+exports.clean = cleanTask;
+exports.generate = generateTask;
+exports.build = series(cleanTask, generateTask);
+exports.deploy = series(cleanTask, generateTask, deployTask);
---
+++ hugo/content/posts/my-first-post.md
@@ -0,0 +1,7 @@
+---
+title: "My First Post"
+date: 2020-03-16T10:56:14-07:00
+draft: false
+---
+
+Hello World!
